06252f36d72b0e593e33233c4a4c26b3980f3846
Gustavo Guimarães <49100132+gustavo234guimaraes@users.noreply.github.com>
2024-05-13 11:26:18 -0300

    Add files via upload

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,23 @@
 
-.onLoad <- function(libname,pkgname){
-  lapply(
-    c('sf', 'rjson', 'data.table', 'tidylog', 'lubridate', 'plotly','scales', 'tidyverse', 'rstudioapi', 'readxl', 'ggrepel', 'ggpmisc', 'hrbrthemes', 'ggpubr', 'shadowtext', 'flextable'),
-    function(x){
-      invisible(
-        suppressPackageStartupMessages(
-          library(x,quietly = TRUE)
-        )
-      )
-    })
-}
 
+.onLoad <- function(libname,pkgname){
+  invisible(
+    suppressMessages(
+      suppressWarnings(
+        
+        lapply(
+          c('sf', 'rjson', 'data.table', 'tidylog', 'lubridate', 'plotly','scales', 'tidyverse', 'rstudioapi', 'readxl', 'ggrepel', 'ggpmisc', 'hrbrthemes', 'ggpubr', 'shadowtext', 'flextable'),
+          function(x){
+            invisible(
+              suppressPackageStartupMessages(
+                library(x,quietly = TRUE,character.only = TRUE)
+              )
+            )
+          })
+        
+      )
+    )
+  )
+  
+  
+}
